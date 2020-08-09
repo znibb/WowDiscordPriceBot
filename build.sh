16e8c3713b2821043e960b132208ea041637971c
znibb <znibb@zkylark.se>
2020-08-09 20:57:32 +0200

    Improved build script to prevent dangling docker images when rebuilding

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker build --no-cache -t znibb/pricebot:latest .
+user=znibb
+name=pricebot:latest
+
+# Remove stopped container
+docker container rm $(docker ps -a | grep $user/$name | awk '{print $1}') > /dev/null 2>&1
+
+# Delete old image to prevent dangling image
+docker image rm $user/$name > /dev/null 2>&1
+
+# Rebuild image
+docker build --no-cache -t $user/$name .
