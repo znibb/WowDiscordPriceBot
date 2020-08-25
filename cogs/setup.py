621f344334ab633c96eec0af9dcf91a7c37607ef
znibb <znibb@zkylark.se>
2020-08-25 14:16:51 +0200

    Updated displayed version number to 1.0.4

--- cogs/setup.py
+++ cogs/setup.py
@@ -2,7 +2,7 @@ import discord
 import requests
 from discord.ext import commands
 
-version="1.0.3"
+version="1.0.4"
 
 class Setup(commands.Cog):
 	baseUrl="https://api.nexushub.co/wow-classic/v1/"
