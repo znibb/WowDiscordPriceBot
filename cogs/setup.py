009aaa97e31250b325a1e348fa4c28d855b632cd
Pontus Petersson <pontus.pson@gmail.com>
2021-11-11 15:03:40 +0100

    Updated displayed version number to 2.0.1

--- cogs/setup.py
+++ cogs/setup.py
@@ -2,7 +2,7 @@ import discord
 import requests
 from discord.ext import commands
 
-version="2.0.0"
+version="2.0.1"
 
 class Setup(commands.Cog):
 	baseUrl="https://api.nexushub.co/wow-classic/v1/"
