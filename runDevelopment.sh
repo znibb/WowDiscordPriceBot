7f507be15953af307005b2a3e87a60066edee332
Pontus Petersson <pontus.pson@gmail.com>
2021-11-27 12:35:44 +0100

    Added provisions for venv

--- runDevelopment.sh
+++ runDevelopment.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [[ -f venv/bin/activate ]]; then
+  echo "Activating virtualenv"
+  source venv/bin/activate
+else
+  echo "Creating virtualenv"
+  python3 -m venv venv
+  source venv/bin/activate
+  pip install -r requirements.txt
+fi
+
 source .env
 export DISCORD_TOKEN
 python3 bot.py
